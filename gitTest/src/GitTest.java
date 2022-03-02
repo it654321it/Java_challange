2a2917a5640832ec139350477c39a1d5f0ce6584
it654321it <xfreeman@ukr.net>
2022-03-02 14:19:10 +0200

    implemented 4th new 'mod' method in init GitTest class

--- gitTest/src/GitTest.java
+++ gitTest/src/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
         int a = 10, b = 3;
         System.out.println("Operation 'multiply' of " + a + " and " + b + " is:" + mul(a, b));
         System.out.println("Operation 'add' of " + a + " and " + b + " is:" + add(a, b));
+        System.out.println("Operation 'module' of " + a + " and " + b + " is:" + mod(a, b));
         System.out.println("Operation 'substruct' of " + a + " and " + b + " is:" + sub(a, b));
     }
 
@@ -17,5 +18,9 @@ public class GitTest {
 
     public static int mul(int a, int b) {
         return a * b;
+    }
+
+    public static int mod(int a, int b) {
+        return a % b;
     }
 }
