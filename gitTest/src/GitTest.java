c079d03751ba947d822e810bb3723ea81da3f5d3
it654321it <xfreeman@ukr.net>
2022-03-02 13:18:13 +0200

    implemented 1st 'add' function in init GitTest class

--- gitTest/src/GitTest.java
+++ gitTest/src/GitTest.java
@@ -1,5 +1,11 @@
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello world !");
+        //System.out.println("Hello world !");  // init version of App
+        int a = 10, b = 3;
+        System.out.println("Operation 'add' of " + a + " and " + b + " is:" + add(a, b));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
