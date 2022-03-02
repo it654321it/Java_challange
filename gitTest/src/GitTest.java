d032e203be29188de9b70483b7a56edf3d637300
it654321it <xfreeman@ukr.net>
2022-03-02 14:30:13 +0200

    implemented 5th new 'max' method in init GitTest class

--- gitTest/src/GitTest.java
+++ gitTest/src/GitTest.java
@@ -1,11 +1,12 @@
 public class GitTest {
     public static void main(String[] args) {
         //System.out.println("Hello world !");  // init version of App
-        int a = 10, b = 3;
-        System.out.println("Operation 'multiply' of " + a + " and " + b + " is:" + mul(a, b));
+        int a = 5, b = 9;
+        System.out.println("Result of 'multiply' operation " + a + " and " + b + " is: " + mul(a, b));
         System.out.println("Operation 'add' of " + a + " and " + b + " is:" + add(a, b));
         System.out.println("Operation 'module' of " + a + " and " + b + " is:" + mod(a, b));
-        System.out.println("Operation 'substruct' of " + a + " and " + b + " is:" + sub(a, b));
+        System.out.println("Operation 'Max' of " + a + " and " + b + " is:" + max(a, b));
+        System.out.println("Operation 'subtract' of " + a + " and " + b + " is:" + sub(a, b));
     }
 
     public static int add(int a, int b) {
@@ -22,5 +23,9 @@ public class GitTest {
 
     public static int mod(int a, int b) {
         return a % b;
+    }
+
+    public static int max(int a, int b) {
+        return a > b ? a : b;
     }
 }
