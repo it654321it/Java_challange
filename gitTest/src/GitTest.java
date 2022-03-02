d6d4c1ca592fbd8715519e40544116d0fba59ef6
it654321it <xfreeman@ukr.net>
2022-03-02 13:35:54 +0200

    implemented 3nd new 'mul' function in init GitTest class

--- gitTest/src/GitTest.java
+++ gitTest/src/GitTest.java
@@ -2,6 +2,7 @@ public class GitTest {
     public static void main(String[] args) {
         //System.out.println("Hello world !");  // init version of App
         int a = 10, b = 3;
+        System.out.println("Operation 'multiply' of " + a + " and " + b + " is:" + mul(a, b));
         System.out.println("Operation 'add' of " + a + " and " + b + " is:" + add(a, b));
         System.out.println("Operation 'substruct' of " + a + " and " + b + " is:" + sub(a, b));
     }
@@ -12,5 +13,9 @@ public class GitTest {
 
     public static int sub(int a, int b) {
         return a - b;
+    }
+
+    public static int mul(int a, int b) {
+        return a * b;
     }
 }
