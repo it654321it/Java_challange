1bd277928019e3af4e2b73ce77c4e9d13e9b4fed
it654321it <xfreeman@ukr.net>
2022-03-02 13:20:37 +0200

    implemented 2nd new 'sub' function in init GitTest class

--- gitTest/src/GitTest.java
+++ gitTest/src/GitTest.java
@@ -3,9 +3,14 @@ public class GitTest {
         //System.out.println("Hello world !");  // init version of App
         int a = 10, b = 3;
         System.out.println("Operation 'add' of " + a + " and " + b + " is:" + add(a, b));
+        System.out.println("Operation 'substruct' of " + a + " and " + b + " is:" + sub(a, b));
     }
 
     public static int add(int a, int b) {
         return a + b;
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
     }
 }
