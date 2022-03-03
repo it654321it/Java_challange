8523e5f79942788592b431b13951191e024d723d
it654321it <xfreeman@ukr.net>
2022-03-03 18:15:40 +0200

    Add 2 variants of App with realization of all SOLID principles and without such (+ class diagrams)

--- All_Solid_Princiles_Realization_Application/src/oldVersionOfApp/AppWithoutSolidPrinciples.java
+++ All_Solid_Princiles_Realization_Application/src/oldVersionOfApp/AppWithoutSolidPrinciples.java
@@ -6,14 +6,15 @@ public class AppWithoutSolidPrinciples {
     public static void main(String[] args) {
         Scanner scaner = new Scanner(System.in);
         System.out.println("Enter 1st number: ");
-        //int firstNum = scaner.nextInt();
-        int firstNum = 4;
+        int firstNum = scaner.nextInt();
         System.out.println("Enter 2nd number: ");
-        //int secondNum = scaner.nextInt();
-        int secondNum = 10;
+        int secondNum = scaner.nextInt();
         
-        int result = firstNum + secondNum;
+        int result = firstNum + secondNum; // variant #1 - summation
+		//int result = firstNum - secondNum; // variant #2 - subtraction
+		//int result = firstNum * secondNum; // variant #3 - multiplying
+		//int result = firstNum / secondNum; // variant #4 - dividing
 
-        System.out.println("Sum of " + firstNum + " and " + secondNum + " is: " + result);
+        System.out.println("Sum (sub/mul/div) of " + firstNum + " and " + secondNum + " is: " + result);
     }
 }
