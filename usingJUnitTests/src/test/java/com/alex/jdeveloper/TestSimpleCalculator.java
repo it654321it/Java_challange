03324bcf01753a64a483f8f218dc00e9653ef03d
it654321it <xfreeman@ukr.net>
2022-03-03 15:40:19 +0200

    Add simple maven-project with 3rd version of JUnit tests

--- usingJUnitTests/src/test/java/com/alex/jdeveloper/TestSimpleCalculator.java
+++ usingJUnitTests/src/test/java/com/alex/jdeveloper/TestSimpleCalculator.java
@@ -8,7 +8,7 @@ class TestSimpleCalculator {
 
     @Test
     void twoMultiplyTwoShouldEqualFour() {
-        SimpleCalculator calculator = new SimpleCalculator();
+        var calculator = new SimpleCalculator();
         
         int num1 = 2, num2 = 2, expected = 4;
 
@@ -17,7 +17,7 @@ class TestSimpleCalculator {
 
     @Test
     void threeMultiplyTenShouldEqualThirty() {
-        SimpleCalculator calculator = new SimpleCalculator();
+        var calculator = new SimpleCalculator();
 
         int num1 = 3, num2 = 10, expected = 30;
 
