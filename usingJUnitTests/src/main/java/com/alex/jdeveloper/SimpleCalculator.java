8563129f69e347050f6d619f171b1125a2d12fd7
it654321it <xfreeman@ukr.net>
2022-03-03 13:50:40 +0200

    Add simple maven-project with JUnit tests

--- usingJUnitTests/src/main/java/com/alex/jdeveloper/SimpleCalculator.java
+++ usingJUnitTests/src/main/java/com/alex/jdeveloper/SimpleCalculator.java
@@ -3,6 +3,6 @@ package com.alex.jdeveloper;
 public class SimpleCalculator {
     public int AddTwoNumbers(int n1, int n2 ) {
         return n1 + n2;  // it gives OK-tests
-        return n1 - n2;  // it gives NOK-tests
+        //return n1 - n2;  // it gives NOK-tests
     }
 }
