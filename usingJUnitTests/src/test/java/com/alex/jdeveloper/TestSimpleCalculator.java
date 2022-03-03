714170959110ce69b094f450acb3cd983293bcbf
it654321it <xfreeman@ukr.net>
2022-03-03 15:35:10 +0200

    Add simple maven-project with 2nd version of JUnit tests

--- usingJUnitTests/src/test/java/com/alex/jdeveloper/TestSimpleCalculator.java
+++ usingJUnitTests/src/test/java/com/alex/jdeveloper/TestSimpleCalculator.java
@@ -1,20 +1,26 @@
 package com.alex.jdeveloper;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestSimpleCalculator {
 
-    @org.junit.jupiter.api.Test
-    void test1() {
-        SimpleCalculator m = new SimpleCalculator();
-        int num1 = 3, num2 = 4, expectedSum = 7;
-        assertEquals(expectedSum, m.AddTwoNumbers(num1, num2));
+    @Test
+    void twoMultiplyTwoShouldEqualFour() {
+        SimpleCalculator calculator = new SimpleCalculator();
+        
+        int num1 = 2, num2 = 2, expected = 4;
+
+        assertTrue(calculator.MultiplyTwoNumbers(num1, num2) == expected);
     }
 
-    @org.junit.jupiter.api.Test
-    void test2() {
-        SimpleCalculator m = new SimpleCalculator();
-        int num1 = 10, num2 = 9, expectedSum = 19;
-        assertEquals(expectedSum, m.AddTwoNumbers(num1, num2));
+    @Test
+    void threeMultiplyTenShouldEqualThirty() {
+        SimpleCalculator calculator = new SimpleCalculator();
+
+        int num1 = 3, num2 = 10, expected = 30;
+
+        assertEquals(expected, calculator.MultiplyTwoNumbers(num1, num2));
     }
 }
